368f1ae3a4b034152ccf4df41eaf083fd49778c9
alspin8 <46360108+alspin8@users.noreply.github.com>
2024-01-20 16:56:07 +0100

    kk

--- packages/o/orion/xmake.lua
+++ packages/o/orion/xmake.lua
@@ -11,7 +11,7 @@ package("orion")
 
     set_urls("https://github.com/alspin8/Orion.git")
 
-    add_versions("1.0a2", "450e46eb6d3437055d1c0bb92d81803fe73adc2f")
+    --add_versions("1.0a2", "450e46eb6d3437055d1c0bb92d81803fe73adc2f")
 
     on_install(function (package)
         import("package.tools.xmake").install(package)
